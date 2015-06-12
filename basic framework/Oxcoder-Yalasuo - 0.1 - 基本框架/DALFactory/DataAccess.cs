7658acdff22b11a59ae70066943f69ab2691be3e
12301041-Qi LIU <12301041@bjtu.edu.cn>
2015-06-12 17:06:26 +0800

    framework
    the basic project framework and database

---
+++ basic framework/Oxcoder-Yalasuo - 0.1 - 基本框架/DALFactory/DataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Configuration;
+
+namespace DALFactory
+{
+    public sealed class DataAccess
+    {
+        // Look up the DAL implementation we should be using
+        //private static readonly string path = ConfigurationManager.AppSettings["WebDAL"];
+        private static readonly string path = "SQLServerDAL";
+        private DataAccess() { }
+
+        /*
+         * 实例化用户数据操作
+         * */
+        public static IDAL.IChallenge CreateChallenge()
+        {
+            string className = "SQLServerDAL.Challenge";
+            return (IDAL.IChallenge)Assembly.Load(path).CreateInstance(className);
+        }
+
+    }
+}
