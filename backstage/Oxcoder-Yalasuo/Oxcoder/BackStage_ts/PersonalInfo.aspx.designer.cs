2581619058e3cb147cba4fa076f86a053839a923
ts87 <345217564@qq.com>
2015-06-26 11:42:14 +0800

    add a web

--- backstage/Oxcoder-Yalasuo/Oxcoder/BackStage_ts/PersonalInfo.aspx.designer.cs
+++ backstage/Oxcoder-Yalasuo/Oxcoder/BackStage_ts/PersonalInfo.aspx.designer.cs
@@ -56,5 +56,32 @@ namespace Oxcoder.BackStage_ts {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Table personalTable;
+        
+        /// <summary>
+        /// HyperLink1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        
+        /// <summary>
+        /// HyperLink2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+        
+        /// <summary>
+        /// HyperLink3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink3;
     }
 }
